bb38af4654bb0d84d742ec79970078e8516dd837
Shivank Kapur <shivankkapur2004@gmail.com>
2024-04-09 21:28:56 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <iframe
+      src="https://www.chatbase.co/chatbot-iframe/3ran-G4YkECrhJ3-_Xw5S"
+      title="RouterGPT"
+      className="router"
+    ></iframe>
+  );
+}
+
+export default App;
